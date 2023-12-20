b4308390faacc80d5e2fe0a6839473f1f3773065
KIMAIS21 <147364549+KIMAIS21@users.noreply.github.com>
2023-12-20 15:46:26 +0300

    Update Лозунговый шифр.py

--- Лозунговый шифр.py
+++ Лозунговый шифр.py
@@ -1,92 +1,95 @@
-def crypt_ru(codeword, inp):
-    alfaR = []
-    alfaR2 = []
+# Функция шифрования вводных данных
+def crypt(inp, alfa, alfa2):
     res = ''
-    for i in range(ord('а'), ord('я') + 1):
-        alfaR.append(chr(i))
-        alfaR2.append(chr(i))
-    for i in range(len(codeword)):
-        alfaR.pop(alfaR.index(codeword[i]))
-        alfaR.insert(i, codeword[i])
     for i in range(len(inp)):
-        res += alfaR[alfaR2.index(inp[i])]
+        res += alfa[alfa2.index(inp[i])]
     return res
 
-
-def decrypt_ru(codeword, inp):
-    alfaR = []
-    alfaR2 = []
+# Функция расшифровки зашифрованных данных
+def decrypt(inp, alfa, alfa2):
     res = ''
-    for i in range(ord('а'), ord('я') + 1):
-        alfaR.append(chr(i))
-        alfaR2.append(chr(i))
-    for i in range(len(codeword)):
-        alfaR.pop(alfaR.index(codeword[i]))
-        alfaR.insert(i, codeword[i])
     for i in range(len(inp)):
-        res += alfaR2[alfaR.index(inp[i])]
-    return res
-
-
-def crypt_EN(codeword, inp):
-    alfaEN = []
-    alfaEN2 = []
-    res = ''
-    for i in range(ord('a'), ord('z') + 1):
-        alfaEN.append(chr(i))
-        alfaEN2.append(chr(i))
-    for i in range(len(codeword)):
-        alfaEN.pop(alfaEN.index(codeword[i]))
-        alfaEN.insert(i, codeword[i])
-    for i in range(len(inp)):
-        res += alfaEN[alfaEN2.index(inp[i])]
-    return res
-
-
-def decrypt_EN(codeword, inp):
-    alfaEN = []
-    alfaEN2 = []
-    res = ''
-    for i in range(ord('a'), ord('z') + 1):
-        alfaEN.append(chr(i))
-        alfaEN2.append(chr(i))
-    for i in range(len(codeword)):
-        alfaEN.pop(alfaEN.index(codeword[i]))
-        alfaEN.insert(i, codeword[i])
-    for i in range(len(inp)):
-        res += alfaEN2[alfaEN.index(inp[i])]
+        res += alfa2[alfa.index(inp[i])]
     return res
 
 
 if __name__ == "__main__":
+
+    # Ввод данных
     codeword = input('Введите кодовое слово: ')
     inp = input('Введите текст для шифрования: ')
     codeword = codeword.lower()
     inp = inp.lower()
 
+    # Массивы под алфавиты
+    alfa =[]
+    alfa2 = []
+
+    # Вывод ошибки
     try:
         pass
     except SyntaxError as e:
         print('Ошибка: ', e)
 
+    # Проверка на язык (ру)
     if ord(inp[0]) in range(ord('а'), ord('я')):
+
+        # Проверка на правильный ввод данных для кодового слова
         for i in range(len(codeword)):
             if ord(codeword[i]) not in range(ord('а'), ord('я')):
-                raise SyntaxError('Неверно задано ключевое слово')
+                if codeword[i] == ' ':
+                    pass
+                else:
+                    raise SyntaxError('Неверно задано ключевое слово')
+
+        # Проверка на правильный ввод данных для текста для шифрования
         for i in range(len(inp)):
             if ord(inp[i]) not in range(ord('а'), ord('я')):
-                raise SyntaxError('Неверно задано ключевое слово')
-        a = crypt_ru(codeword, inp)
-        print(a)
-        print(decrypt_ru(codeword, a))
+                if inp[i] == ' ':
+                    pass
+                else:
+                    raise SyntaxError('Неверно задано ключевое слово')
 
-    if ord(inp[0]) in range(ord('a'), ord('z')):
+        # Добавление элементов массива через таблицу ASCII (Русский)
+        for i in range(ord('а'), ord('я') + 1):
+            alfa.append(chr(i))
+            alfa2.append(chr(i))
+        for i in range(len(codeword)):
+            alfa.pop(alfa.index(codeword[i]))
+            alfa.insert(i, codeword[i])
+
+    # Проверка на язык (ENG)
+    elif ord(inp[0]) in range(ord('a'), ord('z')):
+
+        # Проверка на правильный ввод данных для кодового слова
         for i in range(len(codeword)):
             if ord(codeword[i]) not in range(ord('a'), ord('z')):
-                raise SyntaxError('Неверно задано ключевое слово')
+                if codeword[i] == ' ':
+                    pass
+                else:
+                    raise SyntaxError('Неверно задано кодовое слово')
+
+        # Проверка на правильный ввод данных для текста для шифрования
         for i in range(len(inp)):
             if ord(inp[i]) not in range(ord('a'), ord('z')):
-                raise SyntaxError('Неверно задано ключевое слово')
-        a = crypt_EN(codeword, inp)
-        print(a)
-        print(decrypt_EN(codeword, a))
+                if inp[i] == ' ':
+                    pass
+                else:
+                    raise SyntaxError('Неверно задано кодовое слово')
+
+        # Добавление элементов массива через таблицу ASCII (Анлийский)
+        for i in range(ord('a'), ord('z') + 1):
+            alfa.append(chr(i))
+            alfa2.append(chr(i))
+        for i in range(len(codeword)):
+            alfa.pop(alfa.index(codeword[i]))
+            alfa.insert(i, codeword[i])
+
+    # Добавление в конец алфавита пробел для шифрования больших текстов
+    alfa.append(' ')
+    alfa2.append(' ')
+
+    # Вызов функций
+    a = crypt(inp, alfa, alfa2)
+    print(a)
+    print(decrypt(a, alfa, alfa2))
